b8763a6e0f6b4e3141521de86dfa43529f5af757
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2011-08-11 02:02:42 +0100

    Reorganised, removed other library folders

--- forms.js
+++ forms.js
@@ -1,46 +1,8 @@
-var States = {
-  DEFINED: 'D'
-, IN_PROGRESS: 'P'
-, COMPLETED: 'C'
-, NOT_STARTED: 'N'
-}
-
-var STORY_STATE_CHOICES = [
-  [States.DEFINED,     'Defined']
-, [States.IN_PROGRESS, 'In Progress']
-, [States.COMPLETED,   'Completed']
-]
-
 var TASK_STATE_CHOICES = [
   [States.NOT_STARTED, 'Not Started']
 , [States.IN_PROGRESS, 'In Progress']
 , [States.COMPLETED,   'Completed']
 ]
-
-var ProjectForm = forms.Form({
-  name: forms.CharField({maxLength: 50})
-})
-
-var ReleaseForm = forms.Form({
-  name: forms.CharField({maxLength: 50})
-})
-
-var IterationForm = forms.Form({
-  name: forms.CharField({maxLength: 50})
-})
-
-var StoryForm = forms.Form({
-  name: forms.CharField({maxLength: 255})
-, description: forms.CharField({widget: forms.Textarea})
-, state: forms.ChoiceField({choices: STORY_STATE_CHOICES})
-, blocked: forms.BooleanField({default: false})
-, planned: forms.DecimalField({minValue: 0})
-, owner: forms.ChoiceField()
-, notes: forms.CharField({widget: forms.Textarea})
-, postInit: function(kwargs) {
-    this.fields.owner.setChoices(kwargs.ownerUsers)
-  }
-})
 
 var TaskForm = forms.Form({
   name: forms.CharField({maxLength: 255})
@@ -52,7 +14,4 @@ var TaskForm = forms.Form({
 , todo: forms.DecimalField({minValue: 0})
 , owner: forms.ChoiceField()
 , notes: forms.CharField({widget: forms.Textarea})
-, postInit: function(kwargs) {
-    this.fields.owner.setChoices(kwargs.ownerUsers)
-  }
 })
